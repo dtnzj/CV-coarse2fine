7d102cb4cea2fd21890fa471fb56a9969008686e
nzj <dt_nanzhijie@163.com>
2017-07-26 23:14:31 +0800

    Change the editor and re-commitee the codes. Long time pause for this project.

--- pyramids.py
+++ pyramids.py
@@ -1,95 +1,126 @@
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
 
-This is a temporary script file.
-"""
 
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 
-def compare():
+def RemoveDuplicates(loc, sc,th):
+    loc = np.array(loc).T
+    N = len(loc)
+    aa = [0]
+    for i in range(1,N):
+        flag = 0
+        for j in aa:
+            if np.linalg.norm(loc[i]-loc[j]) < th :
+                j = i if sc[i] > sc [j] else j
+                flag =  1
+                break
+        
+        if flag == 0 :
+            aa.append(i)
+
+    return aa
+
+gserdwfwefrgserg
+gserdwfwefrgserg
+sergdwfwefrg
+sgredwfwefergsgrg
+sgredwfwefergsgrg
+
+def MaxScoreMatch(img,tmpl):
     
+    res = cv2.matchTemplate( img, tmpl, cv2.TM_CCOEFF_NORMED )
+    print (res)
+    return np.where( res >= res.max() )
     
-    
-    a = np.array(a)
-    b = np.array(b)
-    np.linalg.norm(a-b,2)
     pass
 
 
 
-pyrLevelMax = 6
 
-#def Py_PatternMatching():
+
+
+if __name__ == '__main__':        
+    pyrLevelMax = 6
+    os.chdir('./CV course2fine')
+    np.set_printoptions(threshold=100)
+    #def Py_PatternMatching():
+        
+    im = cv2.imread ('IMG30.JPG',1) 
+    im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    #    plt.imshow(im),plt.show()
+    #im = im[:,:,(1,2,0)]
+    #    templ_range = np.array([[285,634],[1743,2085]])
+    #    sh = np.array(im.shape[:2])
+    #    templ_range = templ_range.T / sh.T * np.ones((2,1))
     
-im = cv2.imread ('IMG30.JPG',1) 
-im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-#    plt.imshow(im),plt.show()
-#im = im[:,:,(1,2,0)]
-#    templ_range = np.array([[285,634],[1743,2085]])
-#    sh = np.array(im.shape[:2])
-#    templ_range = templ_range.T / sh.T * np.ones((2,1))
-
-templ = [im_gray[285:634,1743:2085].copy()]
-img_pyr = [im_gray.copy()]
-
-plt.figure(1)
-plt.subplot(2,3,1)
-plt.imshow(img_pyr[0])
-
-plt.figure(2)
-plt.subplot(2,3,1)
-plt.imshow(templ[0])
-
-for i in range(1,pyrLevelMax):
+    templ = [im_gray[285:634,1743:2085].copy()]
+    img_pyr = [im_gray.copy()]
     
-    img_pyr.append( cv2.pyrDown(img_pyr[i-1]))
     plt.figure(1)
-    plt.subplot(2,3,i+1)
-    plt.imshow(img_pyr[i])
+    plt.subplot(2,3,1)
+    plt.imshow(img_pyr[0])
     
-    
-    templ.append(cv2.pyrDown(templ[i-1]))
     plt.figure(2)
-    plt.subplot(2,3,i+1)
-    plt.imshow(templ[i])
+    plt.subplot(2,3,1)
+    plt.imshow(templ[0])
     
-cv2.waitKey(100) 
-cv2.waitKey(0) 
+    for i in range(1,pyrLevelMax):
+        
+        img_pyr.append( cv2.pyrDown(img_pyr[i-1]))
+    #    plt.figure(1)
+    #    plt.subplot(2,3,i+1)
+    #    plt.imshow(img_pyr[i])
+        
+        
+        templ.append(cv2.pyrDown(templ[i-1]))
+    #    plt.figure(2)
+    #    plt.subplot(2,3,i+1)
+    #    plt.imshow(templ[i])
+    for i in range(1,pyrLevelMax):
+        
+        plt.figure(1)
+        plt.subplot(2,3,i+1)
+        plt.imshow(img_pyr[i])        
 
-res = cv2.matchTemplate(img_pyr[i],templ[i],cv2.TM_CCOEFF_NORMED)
-
-w, h = templ[i].shape[::-1]
-loc = np.where( res >= 0.8)
-res[loc[0],loc[1]]
-for pt in zip(*loc[::-1]):
-    cv2.rectangle(img_pyr[i], pt, (pt[0] + w, pt[1] + h), (0,0,255), 1)
+        plt.figure(2)
+        plt.subplot(2,3,i+1)
+        plt.imshow(templ[i])
+        
+    res = cv2.matchTemplate(img_pyr[i],templ[i],cv2.TM_CCOEFF_NORMED)
     
+    w, h = templ[i].shape[::-1]
+    loc = np.where( res >= 0.8)
+    
+    bb = RemoveDuplicates(loc, res[loc].copy(),10)
+#    loc = 
+    loc = np.array(loc).T
+    loc = loc[bb]
+#    loc_T = list(zip(*loc[::-1]))
+#     res[loc[0],loc[1]]
+#    loc = loc[:,::-1]
+    k = 1
+    for pt in loc[:,::-1]:
+        cv2.rectangle(img_pyr[5], tuple(pt), tuple((pt + [w, h])), (0,0,255), 1)
+        tst = img_pyr[4][pt[1]*2:(pt[1]+w+1)*2,pt[0]*2:(pt[0]+h+1)*2]
+        
+        
+        pos = MaxScoreMatch(tst,templ[4])
+        
+    
+    for i in range(1,pyrLevelMax):
+        
+        plt.figure(1)
+        plt.subplot(2,3,i+1)
+        plt.imshow(img_pyr[i])        
 
-plt.figure(1)
-plt.subplot(2,3,i+1)
-plt.imshow(img_pyr[i])
-
-#    cv2.namedWindow( 'orgin', cv2.WINDOW_NORMAL )
-#    cv2.imshow('orgin',im) 
-cv2.waitKey(100) 
-cv2.waitKey(0) 
-#    dst = im.copy()
-#    sh = np.int8(im.shape[0]/2, im.shape[1]/2)
-#    print(sh)
-#    dst = cv2.pyrDown(im)
-#    
-#    plt.imshow(dst),plt.show()
-
-#    cv2.namedWindow( 'matched', cv2.WINDOW_NORMAL )
-#    cv2.imshow('matched',dst)
-#    
-
-cv2.destroyAllWindows()
+        plt.figure(2)
+        plt.subplot(2,3,i+1)
+        plt.imshow(templ[i])
     
     
-#cv2.waitKey(0) 
-#Py_PatternMatching()
+    cv2.destroyAllWindows()
+    
