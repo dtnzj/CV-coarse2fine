4213b13fb31bfad1a06e5fb51dd328848c1c584f
nzj <dt_nanzhijie@163.com>
2017-07-27 20:16:10 +0800

    a little change

--- temp.py
+++ temp.py
@@ -1,26 +1,23 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
+#!/usr/bin/env python3
 
-This is a temporary script file.
-"""
-
+import os
 import cv2
 import matplotlib.pyplot as plt
-import os
 import numpy as np
 from time import clock
 
 
-os.chdir('/home/nzj/Desktop')
-os.getcwd()
+# os.chdir('/home/nzj/Desktop')
+# print(os.getcwd())
 
+# print('fawefw')
 img = cv2.imread('mario.jpg',0)
+
 img = img[:,1:225]
-template = img[82:96,72:81]
-print (img.shape)
+template = img[82:96, 72:81]
+print(img.shape)
 w, h = template.shape[::-1]
-print (template.shape)
+print(template.shape)
 
 t_start = clock()
 for i in range(100):
@@ -31,9 +28,10 @@ threshold = 0.8
 loc = np.where( res >= threshold)
 for pt in zip(*loc[::-1]):
     cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-cv2.imwrite('res.png',img)
+# cv2.imwrite('res.png',img)
 
-plt.imshow(img),plt.show()
+print('fawefw',t_start - t_end)
+# plt. imshow(img),plt.show()
 
-print(t_start - t_end)
+
 
