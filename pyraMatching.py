6edb4b2d6137ac4fbbf7a62df5baebeccb84b292
nzj <dt_nanzhijie@163.com>
2017-08-01 17:23:12 +0800

    upgrade the pyramid matching with the new RemoveDuplicates function

--- pyraMatching.py
+++ pyraMatching.py
@@ -6,55 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-# remove the points that closed to each other, the highest scored one left.
-# input:    RD_loc:    points' positions
-#           RD_sc :    scores
-# output      
-def RemoveDuplicates_new(RD_loc, RD_th=50):
-    RD_loc = np.array(RD_loc).T
-    N = len(RD_loc)
-    # RD_aa is the point group list, the first element of every group is the average location
-    RD_aa = [[]]
-    for i in RD_loc:
-        flag = 0
-        for j in RD_aa :
-            if np.linalg.norm(i-j[0]) < RD_th :
-                j.append(i)
-                j[0]= np.mean(j[1::])
-                flag = 1
-                break    
 
-        if flag == 0 :
-            # add a new point group
-            RD_aa.append(i)
-            # insert the average location at the first element
-            RD_aa.insert(0,i)
-    for i in RD_aa:
-        i = [np.max(i, axis=1), np.min(i, axis=1)]
-        pass
-    return RD_aa
-
-
-# remove the points that closed to each other, the highest scored one left.
-# input:    RD_loc:    points' positions
-#           RD_sc :    scores
-# output      
-def RemoveDuplicates(RD_loc, RD_sc, RD_th=50):
-    RD_loc = np.array(RD_loc).T
-    N = len(RD_loc)
-    RD_aa = [0]
-    for i in range(1, N):
-        flag = 0
-        for j in RD_aa:
-            if np.linalg.norm(RD_loc[i]-RD_loc[j]) < RD_th :
-                j = i if RD_sc[i] > RD_sc [j] else j
-                flag = 1
-                break
-        
-        if flag == 0 :
-            RD_aa.append(i)
-
-    return RD_aa
 
 def MaxScoreMatch(img, tmpl):
     
@@ -72,43 +24,107 @@ def MaxScoreMatch(img, tmpl):
     res = cv2.matchTemplate( img, tmpl, cv2.TM_CCOEFF_NORMED )
     # print (res)
     pos = np.where( res >= res.max() )
-    pos = np.array( pos ).T
+    pos = np.array( pos ).T[0]
 
     return pos
 
+# remove the points that closed to each other, the highest scored one left.
+# input:    RD_loc:    points' positions
+#           RD_sc :    scores
+# output      
+def RemoveDuplicates(RD_loc, width, RD_th=50):
+    # RD_aa is the point group list, the first element of every group is the average location
+    RD_aa = [[RD_loc[0], RD_loc[0]]]
+    # print(RD_aa)
+    for i in RD_loc[1::]:
+        flag = 0
+        for j in RD_aa :
+            # print('j=',j[0],'i=',i)
+            if np.linalg.norm(i-j[0]) < RD_th :
+                j.append(i)
+                j[0]= np.mean(j[1::],axis=0)  
+                flag = 1
+                break    
 
-def RemoveDuplicates_test(img, loc, bb):
-    
-    img[loc] = 0
-    cv2.rectangle(img, tuple(loc[0:1]-1), tuple(loc[2:3]+1), (0, 0, 255), 1)
-    
-    cv2.namedWindow('RemoveDuplicates_img_In', cv2.WINDOW_NORMAL)
-    cv2.imshow('RemoveDuplicates_img_In', img)
+        # print(flag)
+        if flag == 0 :
+            # add a new point group and insert the average location at the first element
+            RD_aa.append([i,i])
 
+    RD_bb = []
+    for i in RD_aa:
+        # print('befor=',i)
+        i = np.append(np.min(i, axis=0),(np.max(i, axis=0)+width))
+        i = np.int16(i)
+        print('after=',i)
+        RD_bb = RD_bb + [i.tolist()]
+        
+    # print(RD_bb)
+    return RD_bb# remove the points that closed to each other, the highest scored one left.
+
+
+# input:    RD_loc:    points' positions
+#           RD_sc :    scores
+# output      
+def RemoveDuplicates(RD_loc, width, RD_th=50):
+    # RD_aa is the point group list, the first element of every group is the average location
+    RD_aa = [[RD_loc[0], RD_loc[0]]]
+    # print(RD_aa)
+    for i in RD_loc[1::]:
+        flag = 0
+        for j in RD_aa :
+            # print('j=',j[0],'i=',i)
+            if np.linalg.norm(i-j[0]) < RD_th :
+                j.append(i)
+                j[0]= np.mean(j[1::],axis=0)  
+                flag = 1
+                break    
+
+        # print(flag)
+        if flag == 0 :
+            # add a new point group and insert the average location at the first element
+            RD_aa.append([i,i])
+
+    RD_bb = []
+    for i in RD_aa:
+        # print('befor=',i)
+        i = np.append(np.min(i, axis=0),(np.max(i, axis=0)+width))
+        i = np.int16(i)
+        print('after=',i)
+        RD_bb = RD_bb + [i.tolist()]
+        
+    # print(RD_bb)
+    return RD_bb
 
 
 # matching the full image with the lowest resolution
 def FirstMatching(img_pyr, templ):
     res = cv2.matchTemplate(img_pyr, templ, cv2.TM_CCOEFF_NORMED)
+
     # cv2.waitKey(0)
 
     loc = np.where( res >= 0.8)
-    bb = RemoveDuplicates_new(loc)
-    RemoveDuplicates_test(img, loc, bb)
-
+    loc = np.array(loc).T
+    print('templ.shape=',templ.shape[::-1])
+    bb = RemoveDuplicates(loc,templ.shape[::-1])
+    
+    print(bb)
+    
+    # c = input()
+    
     # print('bb=',bb)
 # #    loc = 
-    loc = np.array(loc).T
-    loc = loc[bb]
-    loc_T = list(zip(*loc[::-1]))
+    # loc = np.array(loc)
+    # loc = loc[bb]
+    # loc_T = list(zip(*loc[::-1]))
 # #    res[loc[0], loc[1]]
-    loc = loc[:, ::-1]
+    # loc = loc[:, ::-1]
     # print('loc=', loc)
-    return res, loc
+    return res, bb
 
 
 if __name__ == '__main__':
-    pyrLevelMax = 3
+    pyrLevelMax = 2
     # os.chdir('./CV course2fine')
     np.set_printoptions(threshold=100)
     # def Py_PatternMatching():
@@ -145,24 +161,23 @@ if __name__ == '__main__':
     # plt.show()
 
     # coarse matching 
-    h, w = templ[0].shape
+    # h, w = templ[0].shape
     # print('templ[0].shape=', templ[0].shape)
     # print('templ[0].shape[::-1]=', templ[0].shape[::-1])
     
     res, loc = FirstMatching(img_pyr[0], templ[0])
-    
+    loc = np.array(loc)
     # cv2.namedWindow('res', cv2.WINDOW_NORMAL)
     # cv2.imshow('res', res)
     # plt.imshow(res), plt.show()
 
-    # k = 1
     # for pt in loc:
-    #     cv2.rectangle(img_pyr[0], tuple(pt), tuple((pt + [w, h])), (0, 0, 255), 1)
+    #     cv2.rectangle(img_pyr[0], tuple(loc[0][0:2][::-1]), tuple(loc[0][2:4][::-1]), (0, 0, 255), 1)
         # tst = img_pyr[4][pt[1]*2:(pt[1]+w+1)*2, pt[0]*2:(pt[0]+h+1)*2]
     
-    # cv2.namedWindow('image2', cv2.WINDOW_NORMAL)
-    # cv2.imshow('image2', img_pyr[0])
-    
+    cv2.namedWindow('image2', cv2.WINDOW_NORMAL)
+    cv2.imshow('image2', img_pyr[0])
+    cv2.waitKey(0)
     # fine matching, improve the resolution    
     posOut = list()
     k = 0
@@ -177,36 +192,36 @@ if __name__ == '__main__':
             # print('i=', i)
             # print(pos[1]*2, (pos[1]+w+5)*2, pos[0]*2, (pos[0]+h+5)*2)
             pos = pos *2
-            h, w = templ[i].shape
-            tst = img_pyr[i][pos[1]:(pos[1]+w+10), pos[0]:(pos[0]+h+10)]
+            print(pos)
+            # h, w = templ[i].shape
+            tst = img_pyr[i][pos[0]:pos[2], pos[1]:pos[3]]
             cv2.namedWindow('tst', cv2.WINDOW_NORMAL)
             cv2.imshow('tst', tst)
             # print(len(tst), len(templ[i]), i)
             
             cv2.waitKey(0)
             abc = MaxScoreMatch(tst, templ[i])
-            # print('abc=', abc)
-            # print('pos=', pos)
-            pos = pos +abc[::-1]
-            # print('pos(after)=', pos)
+            # print('abc=', abc.append(abc))
+            print('pos=', pos)
+            pos[0:2] = pos[0:2]+ abc
+            pos[2:4] = pos[0:2]+ templ[i].shape
+            print('pos(after)=', pos)
             
             # pos = np.int16(pos)
-            pos=pos[0]
+            # pos=pos[0]
 
             # print('pos=', type(pos[0]))
-            
-            pass
+
         posOut.append(pos)
         
         # k=k+1
         # plt.figure(3)
         # plt.subplot(2, 2, k)
         # plt.imshow(tst)
-    k = 1
-    h, w = templ[-1].shape
-    
+    # print(posOut)
     for pt in posOut:
-        cv2.rectangle(im, tuple(pt), tuple((pt + [w, h])), (0, 0, 255), 1)
+        cv2.rectangle(im, tuple(pt[0:2][::-1]), tuple(pt[2:4][::-1]), (0, 0, 255), 1)
+        # cv2.rectangle(img_pyr[0], tuple(pt[0:2][::-1]), tuple(pt[2:4][::-1]), (0, 0, 255), 1)
         # tst = img_pyr[4][pt[1]*2:(pt[1]+w+1)*2, pt[0]*2:(pt[0]+h+1)*2]
     
     cv2.namedWindow('OutPutImage', cv2.WINDOW_NORMAL)
