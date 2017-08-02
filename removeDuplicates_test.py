b8327ac024e66ce13c6ea3161a5d93a31e211aa2
nzj <dt_nanzhijie@163.com>
2017-08-02 17:27:33 +0800

    remove some useless codes

--- removeDuplicates_test.py
+++ removeDuplicates_test.py
@@ -10,8 +10,6 @@ import numpy as np
 #           RD_sc :    scores
 # output      
 def RemoveDuplicates_new(RD_loc, RD_th=100):
-    # RD_loc = np.array(RD_loc)
-    # N = len(RD_loc)
     # RD_aa is the point group list, the first element of every group is the average location
     RD_aa = [[RD_loc[0], RD_loc[0]]]
     # print(RD_aa)
@@ -38,13 +36,13 @@ def RemoveDuplicates_new(RD_loc, RD_th=100):
         # print('after=',i)
         RD_bb = RD_bb + [i.tolist()]
         
-    print(RD_bb)
+    # print(RD_bb)
     return RD_bb
 
 
 def RemoveDuplicates_test(img, loc, bb):
     
-    print("img = ")
+    # print("img = ")
     # for cc in loc:
     #     print(img[cc[0]][cc[1]][1])
     #     img[cc[0]][cc[1]][1] = 0
@@ -53,12 +51,12 @@ def RemoveDuplicates_test(img, loc, bb):
         cv2.circle(img, tuple(cc[0:2]), 10, (0, 0, 255), 1)
     
     for cc in bb:
-        print("cc = ",cc[0:2],cc[2:4])
+        # print("cc = ",cc[0:2],cc[2:4])
         cv2.rectangle(img, tuple(cc[0:2]), tuple(cc[2:4]), (0, 0, 255), 1)
     
     cv2.namedWindow('RemoveDuplicates_img_In', cv2.WINDOW_NORMAL)
     cv2.imshow('RemoveDuplicates_img_In', img)
-    cv2.waitKey(0)
+    
 
 
 if __name__ == '__main__':
@@ -66,16 +64,7 @@ if __name__ == '__main__':
     img[:,:,1] = np.random.randint(0, 255,size=(1000, 1000))
     loc = np.random.randint(0, 1000,size=(100, 2))
     
-    
-    # cv2.namedWindow('RemoveDuplicates_img_In', cv2.WINDOW_NORMAL)
-    # cv2.imshow('RemoveDuplicates_img_In', img)
-    # img[:,:,1:2] = np.zeros([1000,1000])
-    # plt.imshow(img)
-    # plt.show()
-    # print(np.shape(img))
-    # print(loc)
-    # cv2.waitKey(0)
     bb = RemoveDuplicates_new(loc)
     
     RemoveDuplicates_test(img, loc, bb)
-    pass
+    cv2.waitKey(0)
