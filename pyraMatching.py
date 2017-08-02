bdd6b7fd278faf079a27055bd0ac9139e8e2706d
nzj <dt_nanzhijie@163.com>
2017-08-02 17:26:57 +0800

    add the radio parameter to enable the pyramid radio changable, with replacing cv2.prdown() with cv2.resize()

--- pyraMatching.py
+++ pyraMatching.py
@@ -29,40 +29,7 @@ def MaxScoreMatch(img, tmpl):
 # input:    RD_loc:    points' positions
 #           RD_sc :    scores
 # output      
-def RemoveDuplicates(RD_loc, width, RD_th=50):
-    # RD_aa is the point group list, the first element of every group is the average location
-    RD_aa = [[RD_loc[0], RD_loc[0]]]
-    
-    for i in RD_loc[1::]:
-        flag = 0
-        for j in RD_aa :
-            # print('j=',j[0],'i=',i)
-            if np.linalg.norm(i-j[0]) < RD_th :
-                j.append(i)
-                j[0]= np.mean(j[1::],axis=0)  
-                flag = 1
-                break    
-
-        if flag == 0 :
-            # add a new point group and insert the average location at the first element
-            RD_aa.append([i,i])
-
-    RD_bb = []
-    for i in RD_aa:
-        
-        i = np.append(np.min(i, axis=0),(np.max(i, axis=0)+width))
-        i = np.int16(i)
-        
-        RD_bb = RD_bb + [i.tolist()]
-        
-    # print(RD_bb)
-    return RD_bb
-
-
-# input:    RD_loc:    points' positions
-#           RD_sc :    scores
-# output      
-def RemoveDuplicates(RD_loc, width, RD_th=50):
+def RemoveDuplicates(RD_loc, width, RD_th=10):
     # RD_aa is the point group list, the first element of every group is the average location
     RD_aa = [[RD_loc[0], RD_loc[0]]]
     
@@ -83,30 +50,27 @@ def RemoveDuplicates(RD_loc, width, RD_th=50):
     RD_bb = []
     for i in RD_aa:
         
-        i = np.append(np.min(i, axis=0),(np.max(i, axis=0)+width))
+        i = np.append(np.min(i, axis=0)-1,(np.max(i, axis=0)+width)+1)
         i = np.int16(i)
         
         RD_bb = RD_bb + [i.tolist()]
     
     return RD_bb
 
-
-
-
 # matching the full image with the lowest resolution
-def FirstMatching(img_pyr, templ):
+def FirstMatching(img_pyr, templ, thr = 0.8):
     res = cv2.matchTemplate(img_pyr, templ, cv2.TM_CCOEFF_NORMED)
     
-    loc = np.where( res >= 0.8)
+    loc = np.where( res >= thr)
     loc = np.array(loc).T
     
-    bb = RemoveDuplicates(loc,templ.shape[::-1])
+    bb = RemoveDuplicates(loc,templ.shape[::-1], RD_th = templ.shape[0])
     
     return res, bb
 
 
 # the pyramid matching function
-def PyramidTemplatMatching(img, tmpl, pyrLevelMax=3):
+def PyramidTemplatMatching(img, tmpl, pyrLevelMax=3, ratio=0.5, thr = 0.8):
     
     l = len(img.shape)
     if  l>3 or l<2:
@@ -117,10 +81,14 @@ def PyramidTemplatMatching(img, tmpl, pyrLevelMax=3):
     # generate the pyramid images and templates
     templ = [tmpl]
     img_pyr = [img]
+    
     for i in range(1, pyrLevelMax+1):        
-        img_pyr.insert(0, cv2.pyrDown(img_pyr[0]))
-        templ.insert(0, cv2.pyrDown(templ[0]))
+        sh_i  = np.int16(np.array(img_pyr[0].shape[:2])[::-1]*ratio)
+        sh_t  = np.int16(np.array(  templ[0].shape[:2])[::-1]*ratio)
         
+        img_pyr.insert(0, cv2.resize(img_pyr[0],tuple(sh_i)))
+        templ.insert(  0, cv2.resize(  templ[0],tuple(sh_t)))
+    
     # show the pyramid images 
     # for i in range(0, pyrLevelMax):        
     #     plt.figure(1)
@@ -136,13 +104,15 @@ def PyramidTemplatMatching(img, tmpl, pyrLevelMax=3):
     # plt.show()
 
     # coarse matching 
-    res, loc = FirstMatching(img_pyr[0], templ[0])
+    res, loc = FirstMatching(img_pyr[0], templ[0], thr)
     loc = np.array(loc)
     # for pt in loc:
-    #     cv2.rectangle(img_pyr[0], tuple(loc[0][0:2][::-1]), tuple(loc[0][2:4][::-1]), (0, 0, 255), 1)
+    #     cv2.rectangle(img_pyr[0], tuple(pt[0:2][::-1]), tuple(pt[2:4][::-1]), (0, 0, 255), 1)
     
     # cv2.namedWindow('res', cv2.WINDOW_NORMAL)
     # cv2.imshow('res', res)
+    # plt.imshow(res), plt.show()
+
     # cv2.namedWindow('image2', cv2.WINDOW_NORMAL)
     # cv2.imshow('image2', img_pyr[0])
     # cv2.waitKey(0)
@@ -159,7 +129,7 @@ def PyramidTemplatMatching(img, tmpl, pyrLevelMax=3):
         pos = pt
         for i in range(1, pyrLevelMax+1):  
             
-            pos = pos *2
+            pos = np.int16(pos /ratio)
             tst = img_pyr[i][pos[0]:pos[2], pos[1]:pos[3]]
             # cv2.namedWindow('tst', cv2.WINDOW_NORMAL)
             # cv2.imshow('tst', tst)
@@ -178,8 +148,6 @@ def PyramidTemplatMatching(img, tmpl, pyrLevelMax=3):
 
 
 if __name__ == '__main__':
-    pyrLevelMax = 2
-    
     # np.set_printoptions(threshold=100)
         
     im = cv2.imread ('IMG30.JPG', 1) 
@@ -188,9 +156,9 @@ if __name__ == '__main__':
     templ = im_gray[285:634, 1743:2085]
     
     t_start = clock()
-    posOut = PyramidTemplatMatching(im_gray, templ, pyrLevelMax=3)
+    posOut = PyramidTemplatMatching(im_gray, templ, pyrLevelMax=3, ratio=0.3)
     t_end = clock()
-    dt = t_start - t_end
+    dt = t_end - t_start
     print('fawefw',dt, 1/dt)
 
     for pt in posOut:
