f45a32045c10abbadb380b414189af779548f0b6
dtnzj <dt_nanzhijie@163.com>
2018-05-07 22:04:05 +0800

    give a test client for TCPsocket server

--- framework/TCPsocket_client.py
+++ framework/TCPsocket_client.py
@@ -1,21 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: nzj
+"""
 
 
-import SocketServer
+import socket
+import sys
 
-#定义request handler类，从BaseRequestHandler类继承
-class MyTCPHandler(SocketServer.BaseRequestHandler): 
-    #复写handle()方法，注意：该方法必须复写，用于处理当前的request
-    def handle(self): 
-        #self.request是和客户端连接的套接字，可直接使用
-        self.data = 'LocationRequest'
-        print(self.data)
-        self.request.sendall(self.data)
+HOST, PORT = "localhost", 9999
 
+# Create a socket (SOCK_STREAM means a TCP socket)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+try:
+    # Connect to server and send data
+    sock.connect((HOST, PORT))
+    sock.sendall(b"LocationRequest")
 
-if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999
-    #传入监听地址、端口号和request handler类
-    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler) 
-    #启动监听处理request
-    server.serve_forever() 
+finally:
+    sock.close()
